3baad389820bb0b280c48fa41e3d26bf0a8ad565
Christine Macharia <christineolivia7gmail.com>
2025-03-10 01:17:21 -0700

    Updated navbar.js with a logout modal that asks the user to confirm before they logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import UserContext from "../UserContext";
+import UserContext from "./UserContext";
 
 function NavBar({ logout }) {
   const { currentUser } = useContext(UserContext);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleLogout() {
+    logout();
+    setShowModal(false);
+  }
 
   return (
     <nav>
@@ -15,13 +21,21 @@ function NavBar({ logout }) {
         <>
           <span>Welcome, {currentUser.username}!</span>
           <Link to="/profile">Profile</Link>
-          <Link to="/" onClick={logout}>Logout</Link>
+          <button onClick={() => setShowModal(true)}>Logout</button>
         </>
       ) : (
         <>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </>
+      )}
+
+      {showModal && (
+        <div className="modal">
+          <p>Are you sure you want to log out?</p>
+          <button onClick={handleLogout}>Yes, Logout</button>
+          <button onClick={() => setShowModal(false)}>Cancel</button>
+        </div>
       )}
     </nav>
   );
