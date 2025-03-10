a688f35f6b3e11d17085fe60d5c234f737725e0f
Christine Macharia <christineolivia7gmail.com>
2025-03-09 19:47:09 -0700

    Updated the signupform to call the signup() function from App.js and show error messages if signup fails.

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
-import JoblyApi from "../api";
+import { useNavigate } from "react-router-dom";
 
 function SignupForm({ signup }) {
   const [formData, setFormData] = useState({ username: "", password: "", firstName: "", lastName: "", email: "" });
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
 
   function handleChange(evt) {
     const { name, value } = evt.target;
-    setFormData(data => ({ ...data, [name]: value }));
+    setFormData(fData => ({ ...fData, [name]: value }));
   }
 
   async function handleSubmit(evt) {
     evt.preventDefault();
-    let token = await JoblyApi.signup(formData);
-    signup(token);
+    let result = await signup(formData);
+    if (result.success) {
+      navigate("/");
+    } else {
+      setErrors(result.errors);
+    }
   }
 
   return (
@@ -23,6 +29,7 @@ function SignupForm({ signup }) {
       <input name="lastName" value={formData.lastName} onChange={handleChange} placeholder="Last Name" />
       <input name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
       <button>Sign Up</button>
+      {errors.length > 0 && <p>{errors.join(", ")}</p>}
     </form>
   );
 }
