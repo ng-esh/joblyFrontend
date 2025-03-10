82853a03bd8c73d9be1a008e5830ca2dfdb29268
Christine Macharia <christineolivia7gmail.com>
2025-03-10 01:10:13 -0700

    Restricted access to certain pages (e.g., /profile, /companies/:handle) unless the user is logged in and then updated Routes.js to wrap protected pages inside <PrivateRoute>

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,9 +1,15 @@
 import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
-function PrivateRoute({ user, children }) {
-  if (!user) {
+/** Redirects to login if user is not authenticated */
+function PrivateRoute({ children }) {
+  const { currentUser } = useContext(UserContext);
+
+  if (!currentUser) {
     return <Navigate to="/login" />;
   }
+
   return children;
 }
 
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -4,27 +4,23 @@ import CompanyList from "./CompanyList";
 import CompanyDetail from "./CompanyDetail";
 import JobList from "./JobList";
 import LoginForm from "./LoginForm";
-import SignupForm from "./SignUpForm";
+import SignupForm from "./SignupForm";
 import ProfileForm from "./ProfileForm";
 import PrivateRoute from "./PrivateRoute";
 
-function AppRoutes({ login, signup, user }) {
+function AppRoutes({ login, signup }) {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/companies" element={<CompanyList />} />
-      <Route path="/companies/:handle" element={<CompanyDetail />} />
-      <Route path="/jobs" element={<JobList />} />
       <Route path="/login" element={<LoginForm login={login} />} />
       <Route path="/signup" element={<SignupForm signup={signup} />} />
-      <Route
-        path="/profile"
-        element={
-          <PrivateRoute user={user}>
-            <ProfileForm username={user?.username} />
-          </PrivateRoute>
-        }
-      />
+
+      {/* 🔐 Protected Routes Require Authentication */}
+      <Route path="/companies" element={<PrivateRoute><CompanyList /></PrivateRoute>} />
+      <Route path="/companies/:handle" element={<PrivateRoute><CompanyDetail /></PrivateRoute>} />
+      <Route path="/jobs" element={<PrivateRoute><JobList /></PrivateRoute>} />
+      <Route path="/profile" element={<PrivateRoute><ProfileForm /></PrivateRoute>} />
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
